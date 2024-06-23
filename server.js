3bf0d89f199a1f5eb7b6fe7a2e1640d3267a9edd
Sunny kushwaha <sunny.tostech@gmail.com>
2024-06-23 16:34:55 -0400

    cors updated

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://az-proto-1.azurewebsites.net',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }));
+  
 app.use(bodyParser.json());
 
 //Database connection
